5c95df28d172f6775e5af4cb1252a1e8b388e21d
Erica <ericac2700@tuckahoeschools.org>
2024-10-29 08:10:19 -0400

    Update pattern.py

--- pattern.py
+++ pattern.py
@@ -1,1 +1,9 @@
+import turtle
+t = turtle.Turtle()
+t.shape(name="turtle")
+t.speed(0) # 1:slowest, 3:slow, 5:normal, 10:fast, 0:fastest
 
+for i in range(500):
+  t.forward(i)
+  t.left(100)
+  t.right(70)
